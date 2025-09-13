76743085b9bab49b4ba8a9694adf29033f122d4d
Samuel Argueta <jarguetahernandez02@gmail.com>
2025-09-13 11:58:12 -0600

    chore: adding env variables, starting configuration

--- types/types.ts
+++ types/types.ts
@@ -17,3 +17,27 @@ export type TabParamList = {
   Progress: undefined;
   Profile: undefined;
 };
+
+
+
+// types/env.d.ts
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      // Firebase variables
+      EXPO_PUBLIC_FIREBASE_API_KEY: string;
+      EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN: string;
+      EXPO_PUBLIC_FIREBASE_PROJECT_ID: string;
+      EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET: string;
+      EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID: string;
+      EXPO_PUBLIC_FIREBASE_APP_ID: string;
+      EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID: string;
+      
+      // Otras variables que puedas tener
+      EXPO_PUBLIC_API_URL?: string;
+      EXPO_PUBLIC_APP_ENV?: 'development' | 'staging' | 'production';
+    }
+  }
+}
+
+export {};
